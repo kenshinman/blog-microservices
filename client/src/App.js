bb1d0f08707e2d17918213b5290e6e602b728667
Kehinde Orilogbon <archkenny@yahoo.com>
2020-08-17 01:25:11 +0100

    clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import PostCreate from './PostCreate';
-import PostList from './PostList';
+import React from "react";
+import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
 export default () => {
-  return (
-    <div className="container">
-      <h1>Create Post</h1>
-      <PostCreate />
-      <hr />
-      <h1>Posts</h1>
-      <PostList />
-    </div>
-  );
+	return (
+		<div className="container">
+			<h1>Create Post!!!!!!!!!</h1>
+			<PostCreate />
+			<hr />
+			<h1>Posts</h1>
+			<PostList />
+		</div>
+	);
 };
